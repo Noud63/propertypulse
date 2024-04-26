c085dcb7d2179cd883e8d66d9bdf65561b240d7b
Noud63 <noudvandun@gmail.com>
2024-04-26 14:27:51 +0200

    google OAuth

--- utils/request.js
+++ utils/request.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
+//Fetch all properties
 async function fetchProperties() {
   try {
     //handle the case where the domian is not available yet
@@ -17,4 +18,24 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties}
+//Fetch single property
+async function fetchSingleProperty(id) {
+  try {
+    //handle the case where the domian is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+    const res = await fetch(`${apiDomain}/properties/${id}`); // server component fetch req needs the full url
+    if (!res.ok) {
+      throw new Error("Failed to fetch data!");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+
+export { fetchProperties, fetchSingleProperty}
+
