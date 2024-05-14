870a3c104475bc1fdfe4aa108a43250cf49a4e8f
Noud63 <noudvandun@gmail.com>
2024-05-14 13:38:33 +0200

    sharebuttons

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -1,9 +1,37 @@
 import connectDB from "@/config/database";
-import User from "@/models/User";
 import Property from "@/models/Property";
+import User from "@/models/User";
 import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic = "force-dynamic"
+
+//Get /api/bookmarks
+export const GET = async() => {
+      try {
+        await connectDB();
+
+        const sessionUser = await getSessionUser();
+
+        if (!sessionUser || !sessionUser.userId) {
+          return new Response("User ID is required!", { status: 401 });
+        }
+
+        const { userId } = sessionUser;
+
+        //Find user in database
+        const user = await User.findById({ _id: userId });
+
+        // Get user bookmarks
+        const bookmarks = await Property.find({_id: {$in: user.bookmarks}})
+
+        return new Response(JSON.stringify(bookmarks), {status: 200})
+
+      } catch (error) {
+         console.log(error)
+         return new Response("Something went wrong", { status: 500 });
+      }
+} 
+
 
 export const POST = async(request)=> {   // request = body data
     try {
@@ -22,7 +50,7 @@ export const POST = async(request)=> {   // request = body data
         //Find user in database
         const user = await User.findById({_id: userId})
 
-        //Check if user is property is bookmarked
+        //Check if user property is bookmarked
         let isBookmarked = user.bookmarks.includes(propertyId)
 
         let message;
