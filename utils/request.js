61a258dc894a7dc900aac614a13c3d41109124cc
Noud63 <noudvandun@gmail.com>
2024-05-24 15:34:55 +0200

    featured properties

--- utils/request.js
+++ utils/request.js
@@ -4,7 +4,12 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 //Fetch all properties
 async function fetchProperties({ showFeatured = false } = {}) {  // By default set to false or empty object, otherwise you get an error error
  
-    const res = await fetch(`${apiDomain}/properties${showFeatured ? '/featured' : ""}`,{cache: 'no-store'}); // server component fetch req needs the full url http://localhost:3000/api
+    const res = await fetch(
+      `https://propertypulse-pi.vercel.app/api/properties${
+        showFeatured ? "/featured" : ""
+      }`,
+      { cache: "no-store" }
+    ); // server component fetch req needs the full url http://localhost:3000/api
     if (!res.ok) {
       throw new Error("Failed to fetch data!");
     }
