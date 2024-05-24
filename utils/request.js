70a55385ab02e52737af3e513f68d4aa1695186f
Noud63 <noudvandun@gmail.com>
2024-05-24 13:53:39 +0200

    featured properties

--- utils/request.js
+++ utils/request.js
@@ -4,7 +4,9 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 async function fetchProperties({ showFeatured = false } = {}) {  // By default set to false or empty object, otherwise you get an error
   
     //handle the case where the domian is not available yet
-    
+    if (!apiDomain) {
+      return [];
+    }
     const res = await fetch(`${apiDomain}/properties${showFeatured ? '/featured' : ""}`,{cache: 'no-store'}); // server component fetch req needs the full url http://localhost:3000/api
     if (!res.ok) {
       throw new Error("Failed to fetch data!");
